a0909dfca7ca1157a6f054cb033257ad92f970b6
Sahraoui Larbi <sahraouilarbi@gmail.com>
2026-04-12 14:49:47 +0100

    fix(print): use relative path for cnas logo to ensure it displays correctly in electron build

--- client/src/components/BlueHeader.jsx
+++ client/src/components/BlueHeader.jsx
@@ -6,7 +6,7 @@ export const BlueHeader = () => (
   <div className="cnas-bg-blue mb-2 flex justify-between items-center">
     <div className="w-14 h-14"></div>
     <div className="bg-white p-1 rounded-full w-14 h-14 flex items-center justify-center">
-      <img src="/img/cnas_logo.png" alt="CNAS" className="w-12" />
+      <img src="./img/cnas_logo.png" alt="CNAS" className="w-12" />
     </div>
     <div className="text-center flex-grow">
       <div className="arabic-text text-[11px] font-bold leading-tight">
